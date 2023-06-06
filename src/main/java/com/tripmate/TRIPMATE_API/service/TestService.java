06cde3e46ad36c5005072cafab160a04bc7b3e37
ce1101 <pea1101ce@gmail.com>
2023-06-06 16:22:53 +0900

    User

--- src/main/java/com/tripmate/TRIPMATE_API/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package com.tripmate.TRIPMATE_API.service;
-
-import com.tripmate.TRIPMATE_API.model.User;
-
-import java.util.List;
-
-public interface TestService {
-    public List<User> getAllUser();
-}
